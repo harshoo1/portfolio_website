96383fd2b06b4d695e74b3931b42aebbc9cbc903
Harsh Kumar Sharma <102005811+harshoo1@users.noreply.github.com>
2023-10-03 16:19:29 +0530

    Add files via upload
    changes made on oct-03-2023

---
+++ portfolio/js.js
@@ -0,0 +1,39 @@
+var crsr=document.querySelector("#cursor");
+var crsrblur=document.querySelector("#cursorblur");
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left=dets.x+"px"
+    crsr.style.top=dets.y+"px"
+    crsrblur.style.left=dets.x- 250+"px"
+    crsrblur.style.top=dets.y-250+"px"
+
+})
+
+
+
+
+gsap.to("#nav",{
+    backgroundColor:"#000", 
+    duration:0.5,
+    height:"100px",
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1 //repeat
+    }
+    
+
+})
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        markers:true,
+        start:"top -25%",
+        end:"top -70%",
+        scrub:2
+    }
+})
